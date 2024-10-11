9b3c74edebac3d610c7d30a9fb3b7918eaeaa863
GyuchanMoon <gyuchanm@gmail.com>
2024-10-11 10:18:49 +0900

    CommentRepository update

--- src/main/java/com/estsoft/springdemoproject/repository/CommentRepository.java
+++ src/main/java/com/estsoft/springdemoproject/repository/CommentRepository.java
@@ -2,6 +2,7 @@ package com.estsoft.springdemoproject.repository;
 
 import com.estsoft.springdemoproject.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,5 +13,10 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     Optional<Comment> findByIdAndIsDeleted(Long commentId, Boolean isDeleted);
 
+    // 댓글이 제일많이 달린 게시글을 조회하는 쿼리. 댓글이 가장많은 순서대로 정렬
+    // select post_id, count(1) from comment group by post_id order by count(1) desc
+    // postId: 1, count: 6
+    // HQL -> ORM, JPA entity hibernate. hibernate query language HQL -> sql + entity
+    @Query("SELECT comm.post, COUNT(comm) FROM Comment comm GROUP BY comm.post ORDER BY COUNT(comm) DESC")
     List<Object[]> findCommentCountByPost();
 }
